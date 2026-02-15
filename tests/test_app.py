f763d659eb4f6c57f9a0040a4410e3f195d71f72
EdivarCr <edivarfilho16@gmail.com>
2026-02-15 13:44:12 -0300

    feat: confteste adicionaod, test de post adicionado

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,21 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-# def test_read_root_retorna_ok():
-#     client  = TestClient(app) # arrange (preparação)
-#     response = client.get('/') # act (ação)
-#     assert response.status_code == HTTPStatus.OK # assert (verificação)
-#     assert response.json() == {'message': 'Hello World'}
-
-
-def test_read_root_html_retorna_ok():
-    client = TestClient(app)  # arrange
+def test_read_root_html_retorna_ok(client):
+    # client = TestClient(app)  # arrange
     response = client.get('/')  # act
 
     assert response.status_code == HTTPStatus.OK
- 
+
+
+def test_creat_user_retona_ok(client):
+
+    response = client.post(
+        '/Users/',
+        json={'username': 'bob', 'password': '123', 'email': 'bob123@gmail.com'},
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {'id': 1, 'username': 'bob', 'email': 'bob123@gmail.com'}
+
+
