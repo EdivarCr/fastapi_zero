35b45ef7a3fb6ab574588356e4f8c5921dcb04cc
EdivarCr <edivarfilho16@gmail.com>
2026-02-19 18:12:11 -0300

    feat: test de criacao de userDb

---
+++ tests/test_db.py
@@ -0,0 +1,25 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fastapi_zero.models import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        user = User(username='testuser', email='teste@teste.com', password='teste123')
+
+        session.add(user)
+        session.commit()
+
+        assert user.username == 'testuser'
+
+        user_query = session.scalar(select(User).where(User.username == 'testuser'))
+
+    assert asdict(user_query) == {
+        'id': 1,
+        'username': 'testuser',
+        'email': 'teste@teste.com',
+        'password': 'teste123',
+        'created_at': time
+    }
